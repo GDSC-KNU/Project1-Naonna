bc0b5ab2069ff0788a54b77c53e23b9689b6230f
JangYunSeong <46878756+JangYunSeong@users.noreply.github.com>
2022-05-17 23:13:48 +0900

    delete

--- src/components/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import RootRouter from '../routes/RootRouter';
-
-// eslint-disable-next-line require-jsdoc
-function App() {
-  return <RootRouter />;
-}
-
-export default App;
